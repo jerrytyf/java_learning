0575d2b2b7e048a3cea3b00c0eeb55a92a0c86fc
mac <mac@MacBook-Pro.local>
2025-09-20 17:33:16 +0800

    同一个包下的类直接调用，创建一个点，计算x+y的值

---
+++ Point2D/CountPoint2D.java
@@ -0,0 +1,7 @@
+public class CountPoint2D
+{
+    public static void main(String[] args) {
+        Point2D pointone = new Point2D(100,200);
+        System.err.println(pointone.x + pointone.y);
+    }
+}
