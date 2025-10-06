2d68bdfbfc1a22a21ba66407d1cc4e30c700fca4
mac <mac@MacBook-Pro.local>
2025-10-06 17:19:12 +0800

    20251006

--- Point2D/CountPoint2D.java
+++ Point2D/CountPoint2D.java
@@ -3,5 +3,6 @@ public class CountPoint2D
     public static void main(String[] args) {
         Point2D pointone = new Point2D(100,200);
         System.err.println(pointone.x + pointone.y);
+        System.err.println(pointone.Getx());
     }
 }
